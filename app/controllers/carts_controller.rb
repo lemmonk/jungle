3c158962d6b91894d8d752a6c798b15cb5e4a2fe
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-04-01 23:26:01 -0700

    add sold out style

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,11 @@
 class CartsController < ApplicationController
 
-  def show
+  def show  
   end
 
   def add_item
     product_id = params[:product_id].to_s
+   
     modify_cart_delta(product_id, +1)
 
     redirect_to :back
@@ -22,6 +23,7 @@ class CartsController < ApplicationController
   def modify_cart_delta(product_id, delta)
     cart[product_id] = (cart[product_id] || 0) + delta
     cart.delete(product_id) if cart[product_id] < 1
+   
     update_cart cart
   end
 
