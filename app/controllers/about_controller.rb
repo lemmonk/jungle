b02b8368150ddd2fc72f253277dc190850b85d41
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-04-05 10:15:52 -0700

    add about page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,8 @@
+class AboutController < ApplicationController
+
+  def index
+    @title = "About Us"
+    @body = "This is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page. It is about us.  All things about us can be found here because, well... this is our about page."
+  end
+
+end
